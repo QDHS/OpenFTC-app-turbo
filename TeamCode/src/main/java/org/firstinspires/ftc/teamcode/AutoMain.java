c73fc8d74c3e7a808e2b6f6ba28a25a761a4cc4a
Bob Cao <bobcaocheng@163.com>
2018-04-20 15:43:42 -0500

    upload code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMain.java
@@ -30,21 +30,22 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="Device Testing Opmode", group="Testing Opmode")
+@TeleOp(name="Teleop Manual (Mecnaum)", group="Testing Opmode")
 //@Disabled
 public class TeleopMain extends OpMode
 {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
+    private DcMotor leftFDrive = null;
+    private DcMotor rightFDrive = null;
+    private DcMotor leftBDrive = null;
+    private DcMotor rightBDrive = null;
 
     /*
      * Code to run ONCE when the driver hits INIT
@@ -56,13 +57,17 @@ public class TeleopMain extends OpMode
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "ml");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        leftFDrive  = hardwareMap.get(DcMotor.class, "lf");
+        rightFDrive = hardwareMap.get(DcMotor.class, "rf");
+        leftBDrive  = hardwareMap.get(DcMotor.class, "lb");
+        rightBDrive = hardwareMap.get(DcMotor.class, "rb");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftFDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBDrive.setDirection(DcMotor.Direction.REVERSE);
 
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
@@ -89,31 +94,41 @@ public class TeleopMain extends OpMode
     @Override
     public void loop() {
         // Setup a variable for each drive wheel to save power level for telemetry
-        double leftPower;
-        double rightPower;
+        double leftFPower;
+        double leftBPower;
+        double rightFPower;
+        double rightBPower;
 
         // Choose to drive using either Tank Mode, or POV Mode
         // Comment out the method that's not used.  The default below is POV.
 
         // POV Mode uses left stick to go forward, and right stick to turn.
         // - This uses basic math to combine motions and is easier to drive straight.
-        double drive = -gamepad1.left_stick_y;
-        double turn  =  gamepad1.right_stick_x;
-        leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+        double drivex = -gamepad1.left_stick_x;
+        double drivey = gamepad1.left_stick_y;
 
-        // Tank Mode uses one stick to control each wheel.
-        // - This requires no math, but it is hard to drive forward slowly and keep straight.
-        // leftPower  = -gamepad1.left_stick_y ;
-        // rightPower = -gamepad1.right_stick_y ;
+        double turn  =  gamepad1.right_stick_x;
+
+        double velocity = (gamepad1.left_stick_button ? 1.0 : 0.75) * (1.0 - gamepad1.left_trigger);
+        double ang_velocity = (gamepad1.right_stick_button ? 1.0 : 0.75) * (1.0 - gamepad1.left_trigger);
+
+        double[] v0 = MechanismUtil.calcv(turn * ang_velocity, drivex, drivey);
+
+        rightFPower   = Range.clip(v0[0] * velocity, -1.0, 1.0);
+        leftFPower    = Range.clip(v0[1] * velocity, -1.0, 1.0);
+        leftBPower    = Range.clip(v0[2] * velocity, -1.0, 1.0);
+        rightBPower   = Range.clip(v0[3] * velocity, -1.0, 1.0);
+
 
         // Send calculated power to wheels
-        leftDrive.setPower(leftPower);
-        rightDrive.setPower(rightPower);
+        leftFDrive.setPower(Math.abs(leftFPower) * leftFPower);
+        rightFDrive.setPower(Math.abs(rightFPower) * rightFPower);
+        leftBDrive.setPower(Math.abs(leftBPower) * leftBPower);
+        rightBDrive.setPower(Math.abs(rightBPower) * rightBPower);
 
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+        telemetry.addData("Motors", "v0 (%.2f), v1 (%.2f), v2 (%.2f), v3 (%.2f)", v0[0], v0[1], v0[2], v0[3]);
     }
 
     /*
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanismUtil.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by aosc on 1/25/18.
+ */
+
+public class MechanismUtil {
+}
